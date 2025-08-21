3e4fc52187b496093bc82af7b35f34feab389ba7
unknown <emmco96@gmail.com>
2025-08-21 01:07:12 +0100

    new

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,7 +5,6 @@ import LoadingSpinner from '../components/LoadingSpinner';
 import GoogleSignInButton from '../components/GoogleSignInButton';
 import { MdPerson, MdEmail, MdPhone, MdLock, MdVisibility, MdVisibilityOff } from 'react-icons/md';
 import { useNotification } from '../components/notificationContext';
-import bgImage from '../../src/assets/bg.png';
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -64,7 +63,7 @@ const Register = () => {
          <div className="w-full max-w-md bg-white shadow-2xl rounded-2xl m-4 p-8 relative z-10">
         <div className="text-center mb-6">
           <img
-            src="../../src/assets/logo.png"
+            src="../../assets/logo.png"
             alt="Zippy Pay Logo"
             className="h-30 w-30 mx-auto rounded-full object-cover shadow"
           />
