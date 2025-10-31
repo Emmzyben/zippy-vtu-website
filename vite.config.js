69423d9dcb680db4625900226876a05e323c043f
unknown <emmco96@gmail.com>
2025-11-01 00:02:50 +0100

    new

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.png'],
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
 })
